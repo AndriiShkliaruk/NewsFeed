8d5fb6696f00c6aaa1f9c0f1df2821642566629b
Andrii Shkliaruk <devshkliaruk@gmail.com>
2022-05-10 19:36:01 +0300

    Initial Commit

---
+++ NewsFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsFeed
+//
+//  Created by Andrii Shkliaruk on 10.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
